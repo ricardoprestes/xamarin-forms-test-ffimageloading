62ba968907871ec267361db7b7055de5c1d9bb2d
Ricardo A. P. Prestes <ricardo@forlogic.net>
2018-07-05 10:56:30 -0300

    Subindo projeto

---
+++ AppImage/AppImage/ViewModels/BaseViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AppImage.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _pageName;
+
+        public string PageName
+        {
+            get { return _pageName; }
+            set { SetProperty(ref _pageName, value); }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+    }
+}
---
+++ AppImage/AppImage/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppImage.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string imageSource;
+
+        public string ImageSource
+        {
+            get { return imageSource; }
+            set { SetProperty(ref imageSource, value); }
+        }
+
+        public MainViewModel()
+        {
+            ImageSource = "http://squad4.eastus.cloudapp.azure.com:81/v1/Users/GetPicture?token=PDgktIUEs0cO81TcADbk&identifier=RHUUQHW7PBGKOHB&maxW=300&maxH=300";
+        }
+
+    }
+}
